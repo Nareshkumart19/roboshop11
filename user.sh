f78659add3693e2f8eb4a76823705d0453cb597e
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-26 23:10:29 +0530

    new file

---
+++ user.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=user
+check_root
+app_setup
+
+nodejs_setup
+systemd_setup
+
+
+print_time
+
+
+
+#$mongosh --host mongodb.daws84s.space </app/db/master-data.js &>>$LOG_FILE
+#VALIDATE $? "loding data into mongdb"
+
+#STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("user")')
+#if [ $STATUS -lt 0 ]
+#then
+#    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+#    VALIDATE $? "Loading data into MongoDB"
+#else
+#    echo -e "Data is already loaded ... $Y SKIPPING $N"
+#fi
